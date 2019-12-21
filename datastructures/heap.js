369be8d86b84be4da04a05f5c08e02a52ed5937f
HasheebKazi <hasheeb.k@gmail.com>
2019-12-20 19:36:41 -0500

    heapify implementation

--- datastructures/heap.js
+++ datastructures/heap.js
@@ -1,14 +1,35 @@
-export class MaxHeap {
+const assert = require('assert');
+
+class MaxHeap {
     constructor(arr) {
-        this.heap = this.createHeap(arr);
+        this.heap = this.createMaxHeap(arr);
+        this.size = arr.length;
     }
 
-    createHeap(arr) {
+    createMaxHeap(arr) {
         return arr;
     }
 
-    maxHeapify(arr) {
-        return arr;
+    maxHeapify(index) {
+        // takes an element at an index and bubbles it up
+        let parent = Math.ceil(index/2) - 1;
+        let temp = null;
+        while (parent >= 0) {
+            assert(parent >= 0, 'parent is negative abort');
+            // if child is greater than parent swap them 
+            console.log('i', index);
+            console.log('p', parent);
+
+            parent = Math.ceil(index/2) - 1;
+            if (this.heap[index] > this.heap[parent]) {
+                temp = this.heap[index];
+                this.heap[index] = this.heap[parent];
+                this.heap[parent] = temp;
+                index = parent;
+            } else {
+                break;
+            }
+        }
     }
 
     poll() {
@@ -28,4 +49,13 @@ export class MaxHeap {
     }
 
     
-}                                                                                             
+    
+    toString() {
+        console.log('heap: ', this.heap);
+    }
+}         
+
+module.exports = {
+    MaxHeap
+}
+
