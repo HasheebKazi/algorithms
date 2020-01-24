a2eae363a34b4f6e987f20a199ca49acab01e5cc
HasheebKazi <hasheeb.k@gmail.com>
2020-01-24 16:02:23 -0500

    general heap that can be either a max or min heap

--- datastructures/heap.js
+++ datastructures/heap.js
@@ -1,9 +1,17 @@
 const assert = require('assert');
 
-class MinHeap {
-    constructor(arr) {
-        this.heap = this.createMinHeap(arr);
+const MIN = 0;
+const MAX = 1;
+
+class Heap {
+    constructor(arr, type = MIN) {
+        if (type === MIN) {
+            this.heap = this.createMinHeap(arr);
+        } else {
+            this.heap = this.createMaxHeap(arr);
+        }
         this.size = arr.length;
+        this.type = type;
     }
 
     // naive implementation (nlogn)
@@ -22,6 +30,22 @@ class MinHeap {
         return heap;
     }
 
+    // naive implementation (nlogn)
+    createMaxHeap(arr) {
+        let heap = Array(arr.length).fill({ key: 0, value: null }); // a heap is a complete binary tree (all nodes have two children except for the last level and all nodes are as far left as possible)
+        // representing a complete binary tree with an array is efficient
+        // heap[0] = arr[0];
+        heap[0] = { key: arr[0].key, value: arr[0].value }
+
+        for (let index = 1; index < arr.length; index++) {
+            // heap[index] = arr[index];
+            heap[index] = { key: arr[index].key, value: arr[index].value };
+            this.maxHeapify(heap, index);
+        }
+
+        return heap;
+    }
+
     // logn complexity
     minHeapify(heap, index) {
         // takes an element at an index and bubbles it up
@@ -31,6 +55,26 @@ class MinHeap {
             assert(parent >= 0, 'parent is negative abort');
             // if child is greater than parent swap them and update the index
             if (heap[index].value < heap[parent].value) {
+                temp = heap[index];
+                heap[index] = heap[parent];
+                heap[parent] = temp;
+                index = parent;
+            } else {
+                break;
+            }
+            parent = Math.ceil(index/2) - 1;
+        }
+    }
+
+    // logn complexity
+    maxHeapify(heap, index) {
+        // takes an element at an index and bubbles it up
+        let parent = Math.ceil(index/2) - 1;
+        let temp = null;
+        while (parent >= 0) {
+            assert(parent >= 0, 'parent is negative abort');
+            // if child is greater than parent swap them and update the index
+            if (heap[index].value > heap[parent].value) {
                 temp = heap[index];
                 heap[index] = heap[parent];
                 heap[parent] = temp;
@@ -68,6 +112,8 @@ class MinHeap {
 }         
 
 module.exports = {
-    MinHeap
+    Heap, 
+    MIN,
+    MAX
 }
 
